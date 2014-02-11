e8e39f9b6947e92c51c74bbce9cf60875c942cd1
rightgo09 <skyarrow09@gmail.com>
2014-02-11 21:00:33 +0900

    first commit

---
+++ app.rb
@@ -0,0 +1,102 @@
+# coding: utf-8
+require "sinatra"
+require "sinatra/config_file"
+require "rest_client"
+require "slack-notify"
+require "fileutils"
+require "erb"
+
+config_file './config/settings.yml'
+
+use Rack::Logger
+
+SLACK_TEAM_DOMAIN  = settings.slack["team"]
+SLACK_ACCESS_TOKEN = settings.slack["access_token"]
+SLACK_CHANNEL      = settings.slack["channel"]
+SLACK_USERNAME     = settings.slack["username"]
+
+AZURE_CLIENT_ID     = settings.azure["client_id"]
+AZURE_CLIENT_SECRET = settings.azure["client_secret"]
+
+STORES_FILE             = "./data/words.txt"
+AZURE_ACCESS_TOKEN_FILE = "./data/azure_access_token.txt"
+
+FileUtils.touch STORES_FILE             unless File.exist? STORES_FILE
+FileUtils.touch AZURE_ACCESS_TOKEN_FILE unless File.exist? AZURE_ACCESS_TOKEN_FILE
+
+helpers do
+    def logger
+      request.logger
+    end
+
+  def slack_client
+    @client ||= SlackNotify::Client.new(SLACK_TEAM_DOMAIN, SLACK_ACCESS_TOKEN, {
+      channel: SLACK_CHANNEL,
+      username: SLACK_USERNAME,
+    })
+  end
+
+  def get_microsoft_access_token
+    timestamp, token = File.read(AZURE_ACCESS_TOKEN_FILE).split(",")
+
+    now = Time.now
+    if timestamp && (now.to_i - timestamp.to_i < 590)
+      return token
+    end
+
+    payloads = {
+      "grant_type"    => "client_credentials",
+      "client_id"     => AZURE_CLIENT_ID,
+      "client_secret" => AZURE_CLIENT_SECRET,
+      "scope"         => "http://api.microsofttranslator.com",
+    }
+    url = "https://datamarket.accesscontrol.windows.net/v2/OAuth2-13"
+
+    res = RestClient.post(url, payloads)
+
+    token = JSON.parse(res)["access_token"]
+    File.write(AZURE_ACCESS_TOKEN_FILE, "#{Time.now.to_i},#{token}")
+    token
+  end
+
+  def translate(text)
+    # judge language
+    # ref. http://aoyagikouhei.blog8.fc2.com/blog-entry-163.html
+    if /(?:\p{Hiragana}|\p{Katakana}|[一-龠々])/ === text
+      from = "ja"
+      to   = "en"
+    else
+      from = "en"
+      to   = "ja"
+    end
+    url = "http://api.microsofttranslator.com/v2/Http.svc/Translate?from=#{from}&to=#{to}&text=#{ERB::Util.url_encode(text)}"
+    token = get_microsoft_access_token
+    headers = {"Authorization" => "Bearer #{token}"}
+
+    res = RestClient.get(url, headers)
+    res.force_encoding("UTF-8").sub(/<string.*?">(.*)<\/string>/, '\1')
+  end
+end
+
+post "/" do
+  word = params["text"]
+
+  if word =~ /^@#{SLACK_USERNAME}: .*何と言っている？$/ || word =~ /^@#{SLACK_USERNAME}: translate$/i
+    latest = open("|tail -n 1 < #{STORES_FILE}") { |f| f.gets.chomp }
+
+    translated = translate(latest)
+    logger.info("translated word is #{translated}")
+
+    slack_client.notify(translated)
+  else
+    File.open(STORES_FILE, 'a') do |f|
+      f.puts word
+    end
+  end
+
+  body ""
+end
+
+error do
+  body ""
+end

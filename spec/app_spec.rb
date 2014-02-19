72f16c6a32364e9464a479fd9e2821d144912529
rightgo09 <skyarrow09@gmail.com>
2014-02-19 23:20:55 +0900

    test to post / with invalid team domain

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,9 +1,42 @@
 require "spec_helper"
+require "tempfile"
+
+shared_examples "200 ok" do
+  it "returns 200 ok" do
+    expect(response.status.to_i).to eq(200)
+  end
+end
+
+shared_examples "empty body" do
+  it "returns nothing in HTTP body" do
+    expect(response.body).to be_empty
+  end
+end
 
 describe "GET /" do
   before { get "/" }
   subject(:response) { last_response }
-  it "returns 200 ok" do
-    expect(response.status.to_i).to eq(200)
+  it_behaves_like "200 ok"
+end
+
+describe "POST /" do
+  let(:params) { {team_domain: team_domain, text: text} }
+  let(:words_file) { Tempfile.open("words") }
+  before { ENV["SLACK_WORDS_FILE"] = words_file.path }
+  after { words_file.close! }
+
+  context "invalid team domain" do
+    let(:team_domain) { "hogehoge" }
+    let(:text) { "something" }
+
+    before do
+      ENV["SLACK_TEAM"] = "fugafuga"
+      post "/", params
+    end
+
+    subject(:response) { last_response }
+
+    it_behaves_like "200 ok"
+    it_behaves_like "empty body"
   end
 end
